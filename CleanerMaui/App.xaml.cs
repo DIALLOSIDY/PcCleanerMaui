a3c0b274b1dcbba8ea3fccfa56820c1849cb86d8
diallosidy <diallosidymohamed99@gmail.com>
2024-05-23 23:02:44 +0200

    Ajoutez des fichiers projet.

---
+++ CleanerMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanerMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CleanerMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanerMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
