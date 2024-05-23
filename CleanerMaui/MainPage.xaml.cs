152e8bac5897889ce01f6fa88d199e555d3808c3
diallosidy <diallosidymohamed99@gmail.com>
2024-05-24 00:48:33 +0200

    Decoupage des zones du Logicielet debut du design du header

--- CleanerMaui/MainPage.xaml.cs
+++ CleanerMaui/MainPage.xaml.cs
@@ -9,7 +9,19 @@
             InitializeComponent();
         }
 
-     
+        private async void InfoButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Uri uri = new Uri("http://www.google.com");
+                await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
     }
 
 }
